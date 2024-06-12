1ed62fe5cfc75d73c3cf0c73cde2d5ae21fed770
HarshithaRaj13 <harshitharajshekar13@gmail.com>
2024-06-12 16:38:22 +0530

    Login changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Login from "./Pages/Login";
 
 function App() {
   return (
-    <div className="App">
-    <h1>Welcome to Foodie APP</h1>
-    <h1>Welcome to Foodie APP</h1>
+    <div>
+      <Login />
     </div>
   );
 }
