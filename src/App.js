44f5823380fe0b93d040677adecfad69fe425247
HarshithaRaj13 <harshitharajshekar13@gmail.com>
2024-06-12 17:02:13 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import Login from "./Pages/Login";
-import React from 'react'
-import RouterPage from './Pages/RouterPage'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import RouterPage from "./Pages/RouterPage";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div>
       <Login />
-<RouterPage/>
+      <RouterPage />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
