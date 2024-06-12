187577a880ea6fd1915c79b4b6aac6f82fccfb8f
manoj-kumar-web-dev <manoj@iprimofocus.com>
2024-06-12 11:03:54 +0530

    ..

--- src/Pages/RouterPage.js
+++ src/Pages/RouterPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './Home';
+import LandingPage from './LandingPage';
 function RouterPage() {
   return (
     <div>
          <BrowserRouter>
    <Routes>
-    <Route path='/' element={<Home/>}/>
+    <Route path='/home' element={<Home/>}/>
+    <Route path='/' element={<LandingPage />}/>
     
    </Routes>
    </BrowserRouter>
