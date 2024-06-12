cc90b5363216e4df0eb7cd43272797e4fd2164be
surendranaik123 <131369740+surendranaik123@users.noreply.github.com>
2024-06-12 17:04:37 +0530

    landingchange

--- src/Components/layout/Header.js
+++ src/Components/layout/Header.js
@@ -1,24 +1,72 @@
 import React from 'react'
+import img from '../../Assets/logo.png'
+import personlogo from '../../Assets/person-circle.svg'
+import '../../Styles/global.css'
+import { Dropdown, DropdownItem, NavLink } from 'react-bootstrap';
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 function Header() {
   return (
     <div>
 
-<div class="container">
+<div style={{marginLeft:"100px",marginRight:"40px"}}>
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 ">
     
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-      <img src='' alt='logo'/>
-        <span class="fs-4">Simple header</span>
+    <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none" style={{marginTop:"-25px",marginLeft:"-90px"}}>
+      <img src={img} alt='logo' style={{height:"100px",width:"100px" }}/>
+        <span class="fs-4" style={{fontFamily:"Scheherazade New",fontStyle:"normal",fontWeight:"700",fontSize:"35px",lineHeight:"45px",color:"#000000",marginLeft:"-10px"}}>Food-Frenzy</span>
       </a>
 
-      <ul class="nav nav-pills  ">
-        <li class="nav-item"><a href="#" class="nav-link " aria-current="page">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-      </ul>
+      <ul className="nav nav-pills mt-negative-30">
+      <li className="nav-item">
+        <a href="/home" className=" nav-link-custom" aria-current="page">Home</a>
+      </li>
+      <li className="nav-item">
+        <a href="/veg" className=" nav-link-custom">Veg</a>
+      </li>
+      <li className="nav-item">
+        <a href="/non-veg" className=" nav-link-custom">Non-Veg</a>
+      </li>
+      <li className="nav-item">
+        <a href="/beverages" className=" nav-link-custom">Beverages</a>
+      </li>
+      <li className="nav-item">
+        <a href="/offers" className=" nav-link-custom">Offers</a>
+      </li>
+      <li className="nav-item">
+        <a href="#" className=" nav-link-custom">About Us</a>
+      </li>
+
+      
+          <li style={{display:"flex",marginLeft:"140px"}}>
+          <img src={personlogo} alt='personlogo' style={{height:"35px",width:"35px",marginTop:"0px" }}/>
+            <NavDropdown style={{ marginTop: "0px" }}   >
+            
+                <DropdownItem href="/login" className="  nav-link-custom">
+                  Login with user credentials
+                </DropdownItem>
+             <hr/>
+                <>
+                  <DropdownItem className="nav-link-custom"  >
+                    MY Cart 
+                  </DropdownItem>
+
+                  <hr/>
+                  <DropdownItem  className="nav-link-custom">
+                    Setting
+                  </DropdownItem>
+                  <hr/>
+                  <DropdownItem className=" nav-link-custom">
+                    Logout
+                  </DropdownItem>
+                </>
+        
+            </NavDropdown>
+          </li>
+    </ul>
+     <div>
+     
+     </div>
     </header>
   </div>
     </div>
