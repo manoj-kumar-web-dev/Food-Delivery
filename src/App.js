323df02782677d6fa1ba6cb1430d733453792b4e
manoj-kumar-web-dev <manoj@iprimofocus.com>
2024-06-12 17:18:44 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Login from "./Pages/Login";
 import RouterPage from "./Pages/RouterPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div>
-      <Login />
       <RouterPage />
     </div>
   );
--- src/Pages/RouterPage.js
+++ src/Pages/RouterPage.js
@@ -4,15 +4,19 @@ import Home from './Home';
 import Header from '../Components/layout/Header';
 import LandingPage from './LandingPage';
 import SignupPage from './Signup';
+import Login from './Login';
+
+
+
 function RouterPage() {
   return (
-    <div>
-       
+    <div>    
  <BrowserRouter>
    <Routes>
     <Route path='/home' element={<Home/>}/>
     <Route path='/' element={<LandingPage />}/>
     <Route path='/Signup' element={<SignupPage/>}/>
+    <Route path='/Signin' element={<Login/>}/>
    
    </Routes>
    </BrowserRouter>
