7e841838261925080d7f3299b3ddbd4bc4f7a057
surendranaik123 <131369740+surendranaik123@users.noreply.github.com>
2024-06-12 11:05:47 +0530

    changes

--- src/Components/layout/Header.js
+++ src/Components/layout/Header.js
@@ -2,7 +2,26 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+
+<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 ">
+    
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+      <img src='' alt='logo'/>
+        <span class="fs-4">Simple header</span>
+      </a>
+
+      <ul class="nav nav-pills  ">
+        <li class="nav-item"><a href="#" class="nav-link " aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+      </ul>
+    </header>
+  </div>
+    </div>
   )
 }
 
--- src/Pages/RouterPage.js
+++ src/Pages/RouterPage.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './Home';
+import Header from '../Components/layout/Header';
 function RouterPage() {
   return (
     <div>
-         <BrowserRouter>
+        <Header/>
+ <BrowserRouter>
    <Routes>
     <Route path='/' element={<Home/>}/>
     
+   
    </Routes>
    </BrowserRouter>
     </div>
